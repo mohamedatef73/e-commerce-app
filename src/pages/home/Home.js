45ddd26bc39bcaf182b9ff0d0ec72737200d12db
mohamed atef <59102121+mohamedatef73@users.noreply.github.com>
2020-09-26 02:36:07 +0200

    fix bug and make some edits

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,6 +1,6 @@
 import React from "react"
 import './Home.scss'
-import { AppstoreTwoTone } from '@ant-design/icons';
+import { Link } from 'react-router-dom'
 
 
 
@@ -10,10 +10,7 @@ function Home() {
         <div className='header'>
 
             <nav className="navbar navbar-expand-lg navbar-light">
-
-                <a className="navbar-brand" href="#">
-                    <AppstoreTwoTone style={{ fontSize: '40px', color: '#08c' }} />
-                </a>
+                <a class="navbar-brand" href="#"> <img src={require('../images/icons8-shopify-96.png')} alt='store icon' /></a>
 
                 <button className="navbar-toggler" type="button" data-toggle="collapse"
                     data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown"
@@ -22,30 +19,32 @@ function Home() {
                     <span className="navbar-toggler-icon"></span>
                 </button>
 
-                <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                <div className="collapse navbar-collapse justify-content-center" id="navbarNavDropdown">
                     <ul className="navbar-nav">
                         <li className="nav-item active">
-                            <a className="nav-link" href="#">About</a>
+                            <a className="nav-link text-warning" href="#">About</a>
                         </li>
                         <li className="nav-item active">
-                            <a className="nav-link" href="#">Shopping Cart</a>
+                            <a className="nav-link text-warning" href="#">Shopping Cart</a>
                         </li>
-                        <li className="nav-item active">
-                            <a className="nav-link" href="#">Contacts</a>
+                        <li className="nav-item active ">
+                            <Link to='/contact' className="nav-link text-warning" >Contact Us
+                            </Link>
                         </li>
-                        <li className="nav-item dropdown active">
 
-                            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink"
-                                role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <li className="nav-item dropdown active">
+                            <a className="nav-link dropdown-toggle text-warning" href="#"
+                                data-toggle="dropdown" >
                                 Products Categories
                             </a>
 
-                            <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <div className="dropdown-menu">
                                 <a className="dropdown-item" href="#">choes</a>
                                 <a className="dropdown-item" href="#">Supplements</a>
                                 <a className="dropdown-item" href="#">Gym tools</a>
                             </div>
                         </li>
+
                     </ul>
                 </div>
             </nav>
